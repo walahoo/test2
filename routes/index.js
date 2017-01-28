614f4534567380798842664df8f89c1b235ccaf4
walahoo <rhuang@wellesley.edu>
2017-01-28 14:15:51 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+
+// var Flickr = require("flickrapi"),
+//      flickrOptions = {
+//       api_key: "cfbc7c7fe7cbc19aae015eea0cfd2561",
+//       secret: "db5c9a3244798798",
+//       user_id: "146808703@N04",
+//       access_token: "72157677813892920-a99786a41a9106ed",
+//       access_token_secret: "a0dc7d63541fe739"
+//     };
+// var APIkey = "cfbc7c7fe7cbc19aae015eea0cfd2561";
+// var photoSizesInfo = {
+//     photos: []
+// };
+
+var app = express();
+
 
 //get homepage
 router.get('/', ensureAuthenticated, function(req,res){
@@ -14,5 +37,8 @@ function ensureAuthenticated(req,res,next){
 		res.redirect('/users/login');
 	}
 }
+
+var map = require('./map');
+router.use('/map', map);
 
 module.exports = router;
